c051a4fb8f42e7ea6b162cc84611ec9a25f0e0e7
scskalicky <scskalicky@gmail.com>
2022-03-19 16:19:11 +1300

    add webhook script

---
+++ automagical_pull.php
@@ -0,0 +1,10 @@
+<?php
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+if ( $_POST['payload'] ) {
+   shell_exec('cd ~public_html/stephenskalicky && git pull');
+   shell_exec('mv -v ~/public_html/stephenskalicky/_site/* ~/public_html/stephenskalicky/
+');
+
+}
+
+# ok need to move this into server and make it autowork then might be ok?
