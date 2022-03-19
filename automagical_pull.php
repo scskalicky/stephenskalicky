694b4b24abc888c31c477de86750ee5848afe684
scskalicky <scskalicky@gmail.com>
2022-03-20 09:39:47 +1300

    alter webhook to grab r_data folder

--- automagical_pull.php
+++ automagical_pull.php
@@ -4,6 +4,7 @@ if ( $_POST['payload'] ) {
    shell_exec('cd ~public_html/stephenskalicky && git pull');
    shell_exec('mv -v ~/public_html/stephenskalicky/_site/* ~/public_html/stephenskalicky/
 ');
+shell_exec('mc -c ~/public_html/stephenskalicky/r_data/* ~/public_html/stephenskalicky/);
 
 }
 
