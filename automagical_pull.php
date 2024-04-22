3cc3d39436b3774cebc5d01e02bc67f8b82cf274
Stephen Skalicky <scskalicky@gmail.com>
2024-04-22 20:17:45 +1200

    Update automagical_pull.php

--- automagical_pull.php
+++ automagical_pull.php
@@ -2,5 +2,5 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 if ( $_POST['payload'] ) {
    shell_exec('git pull');
-   shell_exec('mv -v ~/public_html/stephenskalicky/_site/* ~/public_html/stephenskalicky/');
+   shell_exec('mv -v ~/public_html/stephenskalicky/_site/* ~/var/www/vhosts/stephenskalicky/httpdocs');
 }
